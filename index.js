9592849814a7e297ea43f7555560d10e6187bbaf
Christopher Hugh Kelly <christopher.kelly1997@gmail.com>
2023-06-06 03:14:39 -0600

    different order functions

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers = (arr) => {
+    return [arr[0], arr[1]];
+}
+
+const returnLastTwoDrivers = (arr) => {
+    return [arr[arr.length - 2], arr[arr.length - 1]];
+}
+
+function createFareMultiplier(num)
+{
+    return (fare) => {
+        return fare * num;
+    };
+}
+
+let selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+const fareDoubler = (fare) => createFareMultiplier(2)(fare);
+
+const fareTripler = (fare) => createFareMultiplier(3)(fare);
+
+function selectDifferentDrivers(arrayOfDrivers, funct)
+{
+    return funct(arrayOfDrivers);
+}
